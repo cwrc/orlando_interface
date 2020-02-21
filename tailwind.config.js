932bcc39ce3635c13b2826fe606d79d3d8791c24
Nia Kathoni <nikathone@gmail.com>
2020-02-21 17:21:49 -0500

    Override tailwind colors with the one from theme style guide.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 module.exports = {
   prefix: 'tw-',
   theme: {
-    extend: {},
     colors: {
-
+      gray: {
+        100: '#f6f6f6',
+        200: '#cbcbca',
+        300: '#939392',
+        400: '#696868',
+        500: '#1d1d1d',
+      },
+      red: {
+        100: '#981915',
+        200: '#6d0a18',
+      },
+      blue: {
+        100: '#00649b',
+        200: '#034970',
+      }
     },
+    extend: {},
   },
   variants: {},
   plugins: [],
