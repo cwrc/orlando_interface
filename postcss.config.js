ac0ed6d76f8a6d9007c7981c2c54c6c0dca7d6b6
Nia Kathoni <nikathone@gmail.com>
2020-02-26 14:20:06 -0500

    Added postcss custom properties and nested pliguns.

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,10 @@ module.exports = ctx => ({
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
+    require('postcss-nested'),
+    require('postcss-custom-properties')({
+      preserve: false,
+    }),
     require('autoprefixer')({
       cascade: false
     }),
